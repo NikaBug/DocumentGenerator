0f92599f4bc17022d159924c7aa3a1c815a48e03
Veronika Bahniuk <nika6@DESKTOP-B6S6DIR>
2024-01-13 21:08:15 +0200

    Update IMCR

--- Persistence/InMemoryCommandRepository.cs
+++ Persistence/InMemoryCommandRepository.cs
@@ -1,21 +1,57 @@
 ﻿using Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Persistence
 {
     /// <summary>
-    /// Реалізація інтерфейсу репозиторію
-    /// команд
+    /// In memory command repository
     /// </summary>
     public class InMemoryCommandRepository : ICommandRepository
     {
-        public Task Convert(Template intmp, Template outtmp)
+        private IList<Command> cmd = new List<Command>();
+
+        /// <summary>
+        /// Delete command
+        /// </summary>
+        /// <param name="cmdName">command name</param>
+        /// <returns>The successfully completed task</returns>
+        public Task Delete(string cmdName)
         {
-            throw new NotImplementedException();
+            bool res = false;
+            var item = cmd.FirstOrDefault(c => c.CommandName == cmdName);
+            if (item != null)
+            {
+                res = cmd.Remove(item);
+            };
+            return Task.FromResult(res);
+        }
+
+        /// <summary>
+        /// Get command
+        /// </summary>
+        /// <param name="cmdName">command name</param>
+        /// <returns>The successfully completed task</returns>
+        public Task<IEnumerable<Command>> Get(string cmdName)
+        {
+            var list = new List<Command>();
+            foreach (var it in cmd)
+            {
+                if (it.CommandName == cmdName)
+                {
+                    list.Add(it);
+                }
+            }
+            return Task.FromResult<IEnumerable<Command>>(list);
+        }
+
+        /// <summary>
+        /// Save command
+        /// </summary>
+        /// <param name="command">command name</param>
+        /// <returns>The successfully completed task</returns>
+        public Task Save(Command command)
+        {
+            cmd.Add(command);
+            return Task.CompletedTask;
         }
     }
 }
