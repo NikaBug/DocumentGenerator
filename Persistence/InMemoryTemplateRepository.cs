1d16d23a0f1096825f7387f4380d0d635c6804a5
Veronika Bahniuk <nika6@DESKTOP-B6S6DIR>
2024-01-13 21:00:10 +0200

    Update IMTR

--- Persistence/InMemoryTemplateRepository.cs
+++ Persistence/InMemoryTemplateRepository.cs
@@ -1,31 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain;
+﻿using Domain;
 
 namespace Persistence
 {
     /// <summary>
-    /// Реалізація інтерфейсу репозиторію для
-    /// шаблону
+    /// In Memory Template Repository
     /// </summary>
     public class InMemoryTemplateRepository : ITemplateRepository
     {
+        private IList<Template> tmp = new List<Template>();
+
+        /// <summary>
+        /// Delete template
+        /// </summary>
+        /// <param name="name">template name</param>
+        /// <returns>The successfully completed task</returns>
         public Task Delete(string name)
         {
-            throw new NotImplementedException();
+            bool res = false;
+            var item = tmp.FirstOrDefault(t => t.FileName == name);
+            if (item != null)
+            {
+                res = tmp.Remove(item);
+            }
+            return Task.FromResult(res);
         }
 
+        /// <summary>
+        /// Get template
+        /// </summary>
+        /// <param name="name">template name</param>
+        /// <returns>The successfully completed task</returns>
         public Task<IEnumerable<Template>> Get(string name)
         {
-            throw new NotImplementedException();
+            var list = new List<Template>();
+            foreach (var it in tmp)
+            {
+                if (it.FileName == name)
+                {
+                    list.Add(it);
+                }
+            }
+            return Task.FromResult<IEnumerable<Template>>(list);
         }
 
+        /// <summary>
+        /// Save template
+        /// </summary>
+        /// <param name="template">template</param>
+        /// <returns>The successfully completed task</returns>
         public Task Save(Template template)
         {
-            throw new NotImplementedException();
+            tmp.Add(template);
+            return Task.CompletedTask;
         }
     }
 }
