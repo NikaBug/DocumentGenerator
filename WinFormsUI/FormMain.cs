1cd8c1b825e52dc8badb3088a043cbb751bd84e0
Veronika Bahniuk <nika6@DESKTOP-B6S6DIR>
2024-01-14 12:02:54 +0200

    Update WFUI

--- WinFormsUI/FormMain.cs
+++ WinFormsUI/FormMain.cs
@@ -1,3 +1,4 @@
+using Domain;
 using MaterialSkin;
 using MaterialSkin.Controls;
 
@@ -5,12 +6,16 @@ namespace WinFormsUI
 {
     public partial class FormMain : MaterialForm
     {
-        public FormMain()
+        private ITemplateRepository _templateRepository;
+        private ICommandRepository _commandRepository;
+        public FormMain(ITemplateRepository tempRepository, ICommandRepository cmdRepository)
         {
             InitializeComponent();
             var materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            _templateRepository = tempRepository;
+            _commandRepository = cmdRepository;
         }
     }
 }
--- WinFormsUI/Program.cs
+++ WinFormsUI/Program.cs
@@ -1,9 +1,14 @@
+using Domain;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Persistence;
+
 namespace WinFormsUI
 {
     internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
@@ -11,7 +16,23 @@ namespace WinFormsUI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(mainForm: new FormMain());
+            var host = CreateHostBuilder().Build();
+            ServiceProvider = host.Services;
+            Application.Run(ServiceProvider.GetRequiredService<FormMain>());
         }
+
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        static IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                  .ConfigureServices((context, services) =>
+                  {
+                      services.AddTransient<ITemplateRepository, InMemoryTemplateRepository>();
+                      services.AddTransient<ICommandRepository, InMemoryCommandRepository>();
+                      services.AddTransient<FormMain>();
+                  });
+        }
+
     }
 }
