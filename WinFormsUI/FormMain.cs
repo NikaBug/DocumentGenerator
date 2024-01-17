0bd3927dc7091411b92f709d8cc192f3d93f5b18
Veronika Bahniuk <nika6@DESKTOP-B6S6DIR>
2024-01-17 15:08:32 +0200

    Update FormMain.cs

--- WinFormsUI/FormMain.cs
+++ WinFormsUI/FormMain.cs
@@ -1,34 +1,16 @@
-using Domain;
 using MaterialSkin;
 using MaterialSkin.Controls;
-using Presentation.Models;
-using Presentation.Presenters;
-using Presentation.Views;
-using System.Diagnostics;
 
 namespace WinFormsUI
 {
     public partial class FormMain : MaterialForm
     {
-        private ITemplateRepository _templateRepository;
-        private ICommandRepository _commandRepository;
-        public FormMain(ITemplateRepository tempRepository, ICommandRepository cmdRepository)
+        public FormMain()
         {
             InitializeComponent();
             var materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
-            _templateRepository = tempRepository;
-            _commandRepository = cmdRepository;
-        }
-
-        private void materialButtonLoadTemplate_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void materialButtonRemoveTemplate_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
