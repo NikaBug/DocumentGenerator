5c93f724671b6c880c3defa447a2bb7c0e56f157
Veronika Bahniuk <nika6@DESKTOP-B6S6DIR>
2024-01-21 13:51:03 +0200

    update Presentation (extensions)

---
+++ Presentation/ExtensionsPresentation.cs
@@ -0,0 +1,13 @@
+﻿using Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Presentation
+{
+    public static class ExtensionsPresentation
+    {
+        private static void UseCore(IServiceCollection services)
+        {
+            services.AddInMemoryRepositories();
+        }
+    }
+}
