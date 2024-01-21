ede74ebdde9f10d2ef03b00df25e40bd115ef848
Veronika Bahniuk <nika6@DESKTOP-B6S6DIR>
2024-01-21 12:35:09 +0200

    update core (rename extensions class)

---
+++ Core/ExtensionsCore.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+
+namespace Core
+{
+    public static class ExtensionsCore
+    {
+        public static IServiceCollection AddInMemoryRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ITemplateRepository, InMemoryTemplateRepository>();
+            services.AddScoped<ICommandRepository, InMemoryCommandRepository>();
+            return services;
+        }
+    }
+}
