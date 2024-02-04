d06ad8cbedd0c9821ca3715df8cea44d5cebd0d1
Veronika Bahniuk <nika6@DESKTOP-B6S6DIR>
2024-02-04 15:02:16 +0200

    Update Core/Services

---
+++ Core/Services/TemplateService.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+
+namespace Core.Services
+{
+    public class TemplateService
+    {
+        private readonly ITemplateRepository templateRepository;
+
+        public TemplateService(ITemplateRepository templateRepository)
+        {
+            this.templateRepository = templateRepository;
+        }
+
+        public Task<IEnumerable<Template>> GetAllCommands()
+        {
+            return templateRepository.Get(string.Empty);
+        }
+    }
+}
